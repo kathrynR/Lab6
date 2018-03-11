5f55f28ba53307ba8e0dfc076999d803155efb48
kathrynR <37251104+kathrynR@users.noreply.github.com>
2018-03-10 18:13:58 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,11 +1,26 @@
+import java.util.ArrayList;
+
 /*
- * Kate Hohenstein
+ * Author: Kate Hohenstein
+ * Partners: Hannah Hollenback, Kathryn Reese
  * Lab 6
  * 3/10/18
  * Game: set up methods allowing for game execution
  */
 public class Game {
+	/*-----------------------------------------------
+	 * Attributes
+	 * Kathryn: Added Attributes
+	 ------------------------------------------------*/
+	String player;
+	String enemy;
+	ArrayList<Integer> topThreeScores = new ArrayList<Integer>();
 	
+	/*-----------------------------------------------
+	 * Methods
+	 * Kathryn: Added return statements and set up a toString
+	 ------------------------------------------------*/
+
 	//sets up screen
 	public void setUp()
 	{
@@ -22,23 +37,32 @@ public class Game {
 	}
 	
 	//ends game when player/enemy collide
-	public void gameOver()
+	public String gameOver()
 	{
+		return "Game Over";
 	}
 	
 	//display current player score
-	public void showScore()
+	public int showScore()
 	{
+		return 0;
 	}
 	
 	//calculate and store top 3 scores and accompanying player info
-	public void topThree()
-	{
+	// Kathryn 3/10/18: added calculate to the beginning of method name to distinguish it from the other method
+	// Kathryn: Method will add the top three scores to an Arraylist
+	public void calculateTopThree()
+	{	
 	}
 	
 	//display top 3 scores and accompanying player info
-	public void showTopThree()
+	public ArrayList<Integer> showTopThree()
 	{
+		return topThreeScores;
+	}
+	// Kathryn: Added a toString method
+	public String toString() {
+		return "";
 	}
 	
 }
